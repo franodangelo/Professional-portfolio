9dd7df13f1103d6f90d6df6f652e5390e41d4da9
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-08-20 17:17:08 -0300

    min fixes

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,9 +8,9 @@ export default function Header() {
         <header className="w-full h-screen text-center">
             <main className="flex max-w-[1280px] w-full h-full mx-auto p-2 justify-center items-center">
                 <section className="flex flex-col w-[80%]">
-                    <p className="font-light text-base text-slate-800 uppercase tracking-widest">Welcome to my portfolio</p>
-                    <h1 className="font-extrabold text-blue-600 my-4">Hi everyone, I{`"`}m Frano!</h1>
-                    <h4 className="font-light">a <span className="font-bold text-blue-600">Full-Stack Developer</span> based in Buenos Aires, Argentina</h4>
+                    <p className="py-2 font-light text-base text-slate-800 uppercase tracking-widest">Welcome to my portfolio</p>
+                    <h1 className="py-4 font-extrabold text-blue-600">Hi everyone, I{`'`}m Frano!</h1>
+                    <h4 className="py-2 font-light">a <span className="font-bold text-blue-600">Full-Stack Developer</span> based in Buenos Aires, Argentina</h4>
                     <div className="flex max-w-[340px] m-auto mt-4 py-4 items-center justify-between gap-4">
                         <div className="p-4 rounded-full shadow-md shadow-pink-400 cursor-pointer hover:scale-110 hover:shadow-lg hover:shadow-pink-500 ease-in-out duration-200">
                             <Link href="https://www.linkedin.com/in/franco-dangelo/"><FaLinkedinIn /></Link>

f17c16da73009e133af298c889b42be1f6ee8761
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-08-30 11:50:24 -0300

    styled up and linked the contact section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,21 +20,23 @@ export default function Navbar() {
                 <section className="flex">
                     <ul className="hidden md:flex items-center gap-8">
                         <Link offset={-200} href="/#header">
-                            <li className="font-semibold text-sm uppercase hover:text-blue-400 duration-200 cursor-pointer">Home</li>
+                            <li className="font-semibold text-sm uppercase hover:text-blue-600 duration-200 cursor-pointer">Home</li>
                         </Link>
                         <Link href="/#myself">
-                            <li className="font-semibold text-sm uppercase hover:text-blue-400 duration-200 cursor-pointer">Myself</li>
+                            <li className="font-semibold text-sm uppercase hover:text-blue-600 duration-200 cursor-pointer">Myself</li>
                         </Link>
                         <Link href="/#skills">
-                            <li className="font-semibold text-sm uppercase hover:text-blue-400 duration-200 cursor-pointer">Skills</li>
+                            <li className="font-semibold text-sm uppercase hover:text-blue-600 duration-200 cursor-pointer">Skills</li>
                         </Link>
                         <Link href="/#projects">
-                            <li className="font-semibold text-sm uppercase hover:text-blue-400 duration-200 cursor-pointer">Projects</li>
+                            <li className="font-semibold text-sm uppercase hover:text-blue-600 duration-200 cursor-pointer">Projects</li>
                         </Link>
                         <Link href="https://drive.google.com/file/d/1bnItJ1HsNa4XnA-M67K5LFuSu4FZFuEa/view?usp=sharing" download>
-                            <a className="py-2 px-4 font-semibold text-blue-600 uppercase border border-blue-600 rounded-lg shadow-md shadow-blue-200 cursor-pointer hover:scale-105 ease-in-out duration-300" target='_blank'>Download CV</a>
+                            <a className="py-2 px-4 font-semibold text-blue-600 uppercase border border-blue-600 rounded-lg shadow-md shadow-blue-200 cursor-pointer hover:scale-105 ease-in-out duration-300"
+                                target='_blank'>Download CV
+                            </a>
                         </Link>
-                        <Link href="mailto:ffrancodangelo@gmail.com">
+                        <Link href="/#contact">
                             <button className="py-2 px-4 font-semibold text-[#EFF0F3] uppercase bg-blue-600 rounded-lg shadow-md shadow-blue-200 cursor-pointer hover:scale-105 ease-in-out duration-300">Contact me</button>
                         </Link>
                     </ul>
@@ -48,12 +50,13 @@ export default function Navbar() {
             <main className={nav ? "md:hidden fixed lef-0 top-0 w-full h-screen bg-gray-900/90" : null}>
                 {/* This is the blur of the bg */}
                 <div className={nav
-                    ? "md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:[40%] h-screen bg-[#FFFFFE] p-10 ease-in duration-300"
+                    ? "md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:[40%] h-screen bg-[#EFF0F3] p-10 ease-in duration-300"
                     : "fixed left-[-100%] top-0 p-10 ease-out duration-500"}>
                     <section>
                         <div className="flex w-full items-center justify-between">
                             <Image src={logo} alt="logo" width="40px" height="40px" />
-                            <span onClick={handleNav} className="rounded-md shadow-md shadow-900-200 p-2 cursor-pointer">
+                            <span onClick={handleNav}
+                                className="rounded-full shadow-md shadow-900-200 p-2 cursor-pointer">
                                 <HiOutlineX />
                             </span>
                         </div>
@@ -67,22 +70,26 @@ export default function Navbar() {
                         <div className="flex flex-col py-2">
                             <ul>
                                 <Link href="/#header">
-                                    <li onClick={handleNav} className="py-4 font-semibold text-sm text-blue-900 uppercase">
+                                    <li onClick={handleNav}
+                                        className="py-4 font-semibold text-sm text-blue-600 uppercase">
                                         Home
                                     </li>
                                 </Link>
                                 <Link href="/#myself">
-                                    <li onClick={handleNav} className="py-4 font-semibold text-sm text-blue-900 uppercase">
+                                    <li onClick={handleNav}
+                                        className="py-4 font-semibold text-sm text-blue-600 uppercase">
                                         Myself
                                     </li>
                                 </Link>
                                 <Link href="/#skills">
-                                    <li onClick={handleNav} className="py-4 font-semibold text-sm text-blue-900 uppercase">
+                                    <li onClick={handleNav}
+                                        className="py-4 font-semibold text-sm text-blue-600 uppercase">
                                         Skills
                                     </li>
                                 </Link>
                                 <Link href="/#projects">
-                                    <li onClick={handleNav} className="py-4 font-semibold text-sm text-blue-900 uppercase">
+                                    <li onClick={handleNav}
+                                        className="py-4 font-semibold text-sm text-blue-600 uppercase">
                                         Projects
                                     </li>
                                 </Link>
@@ -94,28 +101,43 @@ export default function Navbar() {
                                 <div className="flex w-full my-4 items-center justify-between">
                                     <div className="p-3 rounded-full shadow-sm shadow-blue-100 cursor-pointer hover:scale-105 hover:shadow-md hover:shadow-blue-200 ease-in-out duration-200">
                                         <Link href="/">
-                                            <a href="https://www.linkedin.com/in/franco-dangelo/" target='_blank' rel="noreferrer"><FaLinkedinIn /></a>
+                                            <a href="https://www.linkedin.com/in/franco-dangelo/"
+                                                target='_blank'
+                                                rel="noreferrer">
+                                                <FaLinkedinIn />
+                                            </a>
                                         </Link>
                                     </div>
                                     <div className="p-3 rounded-full shadow-sm shadow-blue-100 cursor-pointer hover:scale-105 hover:shadow-md hover:shadow-blue-200 ease-in-out duration-200">
                                         <Link href="">
-                                            <a href="https://github.com/franodangelo" target='_blank' rel="noreferrer"><FaGithub /></a>
+                                            <a href="https://github.com/franodangelo"
+                                                target='_blank'
+                                                rel="noreferrer">
+                                                <FaGithub />
+                                            </a>
                                         </Link>
                                     </div>
                                     <div className="p-3 rounded-full shadow-sm shadow-blue-100 cursor-pointer hover:scale-105 hover:shadow-md hover:shadow-blue-200 ease-in-out duration-200">
                                         <Link href="/">
-                                            <a href="https://www.behance.net/francodangelo" target='_blank' rel="noreferrer"><FaBehance /></a>
+                                            <a href="https://www.behance.net/francodangelo"
+                                                target='_blank'
+                                                rel="noreferrer">
+                                                <FaBehance />
+                                            </a>
                                         </Link>
                                     </div>
                                 </div>
                             </section>
-                            <button className="w-full mt-8 py-2 px-4 font-semibold text-[#EFF0F3] uppercase bg-gradient-to-l from-blue-400 via-blue-500 to-blue-600 rounded-lg shadow-md shadow-blue-200 cursor-pointer hover:scale-105 ease-in-out duration-300">
+                            <button className="w-full mt-8 py-2 px-4 font-semibold text-blue-600 uppercase border border-blue-600 rounded-lg shadow-md shadow-blue-200 cursor-pointer hover:scale-105 ease-in-out duration-300">
                                 <a href="https://drive.google.com/file/d/1bnItJ1HsNa4XnA-M67K5LFuSu4FZFuEa/view?usp=sharing" download target='_blank' rel="noreferrer">
                                     Download CV
                                 </a>
                             </button>
-                            <Link href="mailto:ffrancodangelo@gmail.com">
-                                <button className="w-full mt-4 py-2 px-4 font-semibold text-[#EFF0F3] uppercase bg-gradient-to-l from-blue-400 via-blue-500 to-blue-600 rounded-lg shadow-md shadow-blue-200 cursor-pointer hover:scale-105 ease-in-out duration-300">Contact me</button>
+                            <Link href="#contact">
+                                <button onClick={handleNav}
+                                    className="w-full mt-4 py-2 px-4 font-semibold text-[#EFF0F3] uppercase bg-gradient-to-l from-blue-400 via-blue-500 to-blue-600 rounded-lg shadow-md shadow-blue-200 cursor-pointer hover:scale-105 ease-in-out duration-300">
+                                    Contact me
+                                </button>
                             </Link>
                         </div>
                     </section>

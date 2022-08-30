8e05d50ed80484a6a69e15a8fba7bbc608834871
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-08-30 12:18:17 -0300

    new minimal footer

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+import { FaChevronUp } from "react-icons/fa";
+
+export default function Footer() {
+    return (
+        <footer className="flex w-full h-20 items-center justify-center border-t border-blue-600 bg-[#EFF0F3]">
+            <h2 className="font-medium text-lg text-blue-600">Thanks for watching! 😀</h2>
+        </footer>
+    )
+}

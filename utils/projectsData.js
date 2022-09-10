31dbf8b27e57686b959eab99ae3ce4cf6502ac90
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-10 16:27:51 -0300

    utils for mapping projects

--- utils/projectsData.js
+++ utils/projectsData.js
@@ -2,7 +2,7 @@ const projects = [
     {
         id: 1,
         name: "Video games Project",
-        thumbnail: "../public/assets/projects/project1.png" ,
+        thumbnail: "/../public/assets/projects/project1.png",
         responsive: "Working on Responsive",
         techs: ["JavaScript", "React", "Node.js", "Express", "PostgreSQL"],
         repo: "https://github.com/franodangelo/Videogames-Proyect",
@@ -11,7 +11,7 @@ const projects = [
     {
         id: 2,
         name: "yumPaw: Pet Care & Shop",
-        thumbnail: "../public/assets/projects/project2.png" ,
+        thumbnail: "/../public/assets/projects/project2.png",
         responsive: "Working on Responsive",
         techs: ["JavaScript", "React", "Node.js", "Express", "PostgreSQL"],
         repo: "https://github.com/franodangelo/yumPaw-Project",
@@ -20,7 +20,7 @@ const projects = [
     {
         id: 3,
         name: "Services SPA",
-        thumbnail: "../public/assets/projects/project3.png" ,
+        thumbnail: "/../public/assets/projects/project3.png",
         responsive: "Responsive",
         techs: ["JavaScript", "React", "Tailwind"],
         repo: "https://github.com/franodangelo/Services-SPA",
@@ -29,10 +29,12 @@ const projects = [
     {
         id: 4,
         name: "Small Weather App",
-        thumbnail: "../public/assets/projects/project4.png" ,
+        thumbnail: "/../public/assets/projects/project4.png",
         responsive: "Working on Responsive",
         techs: ["JavaScript", "Next.js", "Tailwind"],
         repo: "https://github.com/franodangelo/Weather-app",
         deploy: "https://howistheweathernow.vercel.app/"
     }
 ]
+
+export default projects;

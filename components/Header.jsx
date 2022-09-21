1cc59fdbb9fc5f87a3e0f7f0e927df31fffd1523
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-21 13:48:44 -0300

    change the shadow color

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,17 +12,17 @@ export default function Header() {
                     <h1 className="py-4 font-extrabold text-blue-600">Hi everyone, I{`'`}m Franco!</h1>
                     <h4 className="py-2 font-light">a <span className="font-bold text-blue-600">Full-Stack Developer</span> based in Buenos Aires, Argentina 🇦🇷</h4>
                     <div className="flex m-auto my-8 items-end justify-between gap-8 md:gap-16">
-                        <div className="p-4 rounded-full shadow-md shadow-pink-200 cursor-pointer hover:scale-105 hover:shadow-pink-300 ease-in-out duration-200">
+                        <div className="p-4 rounded-full shadow-md shadow-blue-200 cursor-pointer hover:scale-105 hover:shadow-pink-300 ease-in-out duration-200">
                             <Link href="/">
                                 <a href="https://www.linkedin.com/in/franco-dangelo/" target='_blank' rel="noreferrer"><FaLinkedinIn /></a>
                             </Link>
                         </div>
-                        <div className="p-4 rounded-full shadow-md shadow-pink-200 cursor-pointer hover:scale-105 hover:shadow-pink-300 ease-in-out duration-200">
+                        <div className="p-4 rounded-full shadow-md shadow-blue-200 cursor-pointer hover:scale-105 hover:shadow-pink-300 ease-in-out duration-200">
                             <Link href="">
                                 <a href="https://github.com/franodangelo" target='_blank' rel="noreferrer"><FaGithub /></a>
                             </Link>
                         </div>
-                        <div className="p-4 rounded-full shadow-md shadow-pink-200 cursor-pointer hover:scale-105 hover:shadow-pink-300 ease-in-out duration-200">
+                        <div className="p-4 rounded-full shadow-md shadow-blue-200 cursor-pointer hover:scale-105 hover:shadow-pink-300 ease-in-out duration-200">
                             <Link href="">
                                 <a href="https://twitter.com/techfrano" target='_blank' rel="noreferrer"><FaTwitter /></a>
                             </Link>

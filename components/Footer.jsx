9a04841e8aae40d39748d90f65cfe8699ad31e56
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-20 21:19:45 -0300

    last update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,10 @@
-import Link from "next/link";
 import React from "react";
-import { FaChevronUp } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col w-full h-20 gap-2 items-center justify-center border-t border-blue-600 bg-[#EFF0F3]">
+        <footer className="flex flex-col w-full h-20 gap-2 items-center justify-center border-t border-blue-600 bg-white">
             <h2 className="font-medium text-lg text-blue-600">Thanks for watching!😀</h2>
-            <p className="font-thin text-sm text-blue-600">Last update: 14-09-2022</p>
+            <p className="font-thin text-sm text-blue-600">Last update: 20-09-2022</p>
         </footer>
     )
 }

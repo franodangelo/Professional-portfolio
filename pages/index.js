a2425a2a9b3ad5dc449cc078895b1fc812f1ed44
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-10-02 18:58:26 -0300

    wrapped all in language provider

--- pages/index.js
+++ pages/index.js
@@ -6,22 +6,25 @@ import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
+import LanguageProvider from "../context/LanguageContext";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>My portfolio - Franco D{"'"}Angelo</title>
-        <meta name="description" content="I'm a Full Stack Developer looking for my very first experience in IT" />
-        <link rel="icon" href="/logo.ico" />
-      </Head>
-      <Navbar />
-      <Header />
-      <Myself />
-      <Projects />
-      <Skills />
-      <Contact />
-      <Footer />
+      <LanguageProvider>
+        <Head>
+          <title>My portfolio - Franco D{"'"}Angelo</title>
+          <meta name="description" content="I'm a Full Stack Developer looking for my very first experience in IT" />
+          <link rel="icon" href="/logo.ico" />
+        </Head>
+        <Navbar />
+        <Header />
+        <Myself />
+        <Projects />
+        <Skills />
+        <Contact />
+        <Footer />
+      </LanguageProvider>
     </div>
   )
 }

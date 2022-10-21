e46d92f8894122c5a963f13449f9d55be9f7486b
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-10-21 11:20:22 -0300

    change in text sizes in chips, uppercase for responsive and less rounded images

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -21,27 +21,27 @@ export default function Projects() {
                 <section className="grid p-4 grid-cols-1 gap-8 md:grid-cols-3">
                     <div className="flex flex-col h-full w-full p-8 items-center justify-center shadow-md shadow-blue-200 rounded-xl relative cursor-pointer hover:shadow-lg hover:shadow-blue-300 ease-in-out duration-200">
                         <div className="w-full flex justify-between">
-                            <span className="w-fit flex mb-4 py-1 px-2 self-end text-xs text-[#EFF0F3] bg-pink-500 rounded-md uppercase">Updated</span>
+                            <span className="w-fit flex mb-4 py-1 px-2 self-end text-xs text-white bg-pink-500 rounded-md uppercase">Updated</span>
                             <div className="w-full flex justify-end gap-2">
                                 <span className="flex mb-4 py-1 px-2 self-end text-xs text-blue-800 bg-blue-200/50 rounded-md uppercase">API REST</span>
                                 <span className="flex mb-4 py-1 px-2 self-end text-xs text-white bg-blue-600/50 rounded-md uppercase">May 22</span>
                             </div>
                         </div>
-                        <Image className="rounded-xl" src={propertyImg1} alt="project thumbnail" />
+                        <Image className="rounded" src={propertyImg1} alt="project thumbnail" />
                         <div>
                             <div className="pb-4">
                                 <h6 className="mt-4 py-2 text-center border-t-2 border-gray-200">Video games Project</h6>
-                                <p className="font-light text-sm text-center text-blue-500">{texts.projectsResponsive}</p>
+                                <p className="font-light text-xs text-center text-blue-500 uppercase">{texts.projectsResponsive}</p>
                             </div>
                             <div className="flex flex-wrap place-content-center py-4 gap-2">
                                 <span className="px-2 py-1 font-light text-[10px] text-center bg-yellow-500/80 rounded-full">JavaScript</span>
                                 <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-cyan-500/80 rounded-full">React</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-violet-400/80 rounded-full">Redux</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-green-600/80 rounded-full">Node.js</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-gray-200/80 rounded-full">Express</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-blue-700/80 rounded-full">PostgreSQL</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-blue-300/80 rounded-full">Sequelize</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-violet-400/80 rounded-full">Redux</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-green-600/80 rounded-full">Node.js</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-gray-200/80 rounded-full">Express</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-blue-700/80 rounded-full">PostgreSQL</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-blue-300/80 rounded-full">Sequelize</span>
                             </div>
                         </div>
                         <div className="flex mt-4 justify-center items-center gap-4">
@@ -55,17 +55,17 @@ export default function Projects() {
                     </div>
                     <div className="flex flex-col h-auto w-full p-8 items-center justify-center shadow-md shadow-blue-200 rounded-xl relative cursor-pointer hover:shadow-lg hover:shadow-blue-300 ease-in-out duration-200">
                         <span className="flex mb-4 py-1 px-2 self-end text-xs text-white bg-blue-600/50 rounded-md uppercase">Sep 22</span>
-                        <Image className="rounded-xl" src={propertyImg5} alt="project thumbnail" />
+                        <Image className="rounded" src={propertyImg5} alt="project thumbnail" />
                         <div>
                             <div className="pb-4">
                                 <h6 className="mt-4 py-2 text-center border-t-2 border-gray-200">Tennis E-commerce</h6>
-                                <p className="font-light text-sm text-center text-blue-500">{texts.projectsResponsive}</p>
+                                <p className="font-light text-xs text-center text-blue-500 uppercase">{texts.projectsResponsive}</p>
                             </div>
                             <div className="flex flex-wrap place-content-center py-4 gap-2">
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-yellow-500/80 rounded-full">JavaScript</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-cyan-500/80 rounded-full">React</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-yellow-500 rounded-full">Firebase</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-yellow-500/80 rounded-full">JavaScript</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-cyan-500/80 rounded-full">React</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-yellow-500 rounded-full">Firebase</span>
                             </div>
                         </div>
                         <div className="flex mt-4 justify-center items-center gap-4">
@@ -82,16 +82,16 @@ export default function Projects() {
                             <span className="flex mb-4 py-1 px-2 self-end text-xs text-blue-800 bg-blue-200/50 rounded-md uppercase">API REST</span>
                             <span className="flex mb-4 py-1 px-2 self-end text-xs text-white bg-blue-600/50 rounded-md uppercase">{texts.projectsAug} 22</span>
                         </div>
-                        <Image className="rounded-xl" src={propertyImg4} alt="project thumbnail" />
+                        <Image className="rounded" src={propertyImg4} alt="project thumbnail" />
                         <div>
                             <div className="pb-4">
                                 <h6 className="mt-4 py-2 text-center border-t-2 border-gray-200">Small Weather App</h6>
-                                <p className="font-light text-sm text-center text-blue-500">{texts.projectsResponsive}</p>
+                                <p className="font-light text-xs text-center text-blue-500 uppercase">{texts.projectsResponsive}</p>
                             </div>
                             <div className="flex flex-wrap place-content-center py-4 gap-2">
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-yellow-500/80 rounded-full">JavaScript</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-gray-200/80 rounded-full">Next.js</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-yellow-500/80 rounded-full">JavaScript</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-gray-200/80 rounded-full">Next.js</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
                             </div>
                         </div>
                         <div className="flex mt-4 justify-center items-center gap-4">
@@ -105,16 +105,16 @@ export default function Projects() {
                     </div>
                     <div className="flex flex-col h-auto w-full p-8 items-center justify-center shadow-md shadow-blue-200 rounded-xl relative cursor-pointer hover:shadow-lg hover:shadow-blue-300 ease-in-out duration-200">
                         <span className="flex mb-4 py-1 px-2 self-end text-xs text-white bg-blue-600/50 rounded-md uppercase">{texts.projectsAug} 22</span>
-                        <Image className="rounded-xl" src={propertyImg3} alt="project thumbnail" />
+                        <Image className="rounded" src={propertyImg3} alt="project thumbnail" />
                         <div>
                             <div className="pb-4">
                                 <h6 className="mt-4 py-2 text-center border-t-2 border-gray-200">Services SPA</h6>
-                                <p className="font-light text-sm text-center text-blue-500">{texts.projectsResponsive}</p>
+                                <p className="font-light text-xs text-center text-blue-500 uppercase">{texts.projectsResponsive}</p>
                             </div>
                             <div className="flex flex-wrap place-content-center py-4 gap-2">
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-yellow-500/80 rounded-full">JavaScript</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-cyan-500/80 rounded-full">React</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-yellow-500/80 rounded-full">JavaScript</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-cyan-500/80 rounded-full">React</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
                             </div>
                         </div>
                         <div className="flex mt-4 justify-center items-center gap-4">
@@ -131,21 +131,21 @@ export default function Projects() {
                             <span className="flex mb-4 py-1 px-2 self-end text-xs text-blue-800 bg-blue-200/50 rounded-md uppercase">API REST</span>
                             <span className="flex mb-4 py-1 px-2 self-end text-xs text-white bg-blue-600/50 rounded-md uppercase">Jun 22</span>
                         </div>
-                        <Image className="rounded-xl" src={propertyImg2} alt="project thumbnail" />
+                        <Image className="rounded" src={propertyImg2} alt="project thumbnail" />
                         <div>
                             <div className="pb-4">
                                 <h6 className="mt-4 py-2 text-center border-t-2 border-gray-200">yumPaw: Pet Care & Shop</h6>
-                                <p className="font-light text-sm text-center text-gray-500">{texts.projectsNotResponsive}</p>
+                                <p className="font-light text-xs text-center text-gray-500 uppercase">{texts.projectsNotResponsive}</p>
                             </div>
                             <div className="flex flex-wrap place-content-center py-4 gap-2">
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-yellow-500/80 rounded-full">JavaScript</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-cyan-500/80 rounded-full">React</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-violet-400/80 rounded-full">Redux</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-blue-500/80 rounded-full">CSS</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-green-600/80 rounded-full">Node.js</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-gray-200/80 rounded-full">Express</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-blue-700/80 rounded-full">PostgreSQL</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-blue-300/80 rounded-full">Sequelize</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-yellow-500/80 rounded-full">JavaScript</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-cyan-500/80 rounded-full">React</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-violet-400/80 rounded-full">Redux</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-blue-500/80 rounded-full">CSS</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-green-600/80 rounded-full">Node.js</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-gray-200/80 rounded-full">Express</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-blue-700/80 rounded-full">PostgreSQL</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-blue-300/80 rounded-full">Sequelize</span>
                             </div>
                         </div>
                         <div className="flex mt-4 justify-center items-center gap-4">
@@ -167,9 +167,9 @@ export default function Projects() {
                                 <h6 className="py-1 text-center">{`Client's Company Website`}</h6>
                             </div>
                             <div className="flex flex-wrap place-content-center py-2 gap-2">
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-yellow-500/80 rounded-full">JavaScript</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-cyan-500/80 rounded-full">React</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-yellow-500/80 rounded-full">JavaScript</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-cyan-500/80 rounded-full">React</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
                             </div>
                         </div>
                     </div>
@@ -183,9 +183,9 @@ export default function Projects() {
                                 <h6 className="py-1 text-center">Cryptocurrency SPA</h6>
                             </div>
                             <div className="flex flex-wrap place-content-center py-2 gap-2">
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center bg-yellow-500/80 rounded-full">JavaScript</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-cyan-500/80 rounded-full">React</span>
-                                <span className="px-2 py-1 font-light text-[10px] md:text-xs text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-yellow-500/80 rounded-full">JavaScript</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-cyan-500/80 rounded-full">React</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
                             </div>
                         </div>
                     </div>

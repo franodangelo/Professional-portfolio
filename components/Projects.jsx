008849ca38994aaa0868fb7bc352c1dc5bc97ccd
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-10-31 16:16:14 -0300

    height and width for the images with object-cover property in the class

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -18,44 +18,10 @@ export default function Projects() {
             <div className="h-full mx-auto">
                 <h6 className="py-2 font-light text-center text-slate-800 uppercase tracking-widest">✔️ {texts.projectsSection}</h6>
                 <h2 className="py-4 font-extrabold text-center text-blue-600">{texts.projectsH1}</h2>
-                <section className="grid p-4 grid-cols-1 gap-8 md:grid-cols-3">
-                    <div className="flex flex-col h-full w-full p-8 items-center justify-center shadow-md shadow-blue-200 rounded-xl relative cursor-pointer hover:shadow-lg hover:shadow-blue-300 ease-in-out duration-200">
-                        <div className="w-full flex justify-between">
-                            <span className="w-fit flex mb-4 py-1 px-2 self-end text-xs text-white bg-pink-500 rounded-md uppercase">Updated</span>
-                            <div className="w-full flex justify-end gap-2">
-                                <span className="flex mb-4 py-1 px-2 self-end text-xs text-blue-800 bg-blue-200/50 rounded-md uppercase">API REST</span>
-                                <span className="flex mb-4 py-1 px-2 self-end text-xs text-white bg-blue-600/50 rounded-md uppercase">May 22</span>
-                            </div>
-                        </div>
-                        <Image className="rounded" src={propertyImg1} alt="project thumbnail" />
-                        <div>
-                            <div className="pb-4">
-                                <h6 className="mt-4 py-2 text-center border-t-2 border-gray-200">Video games Project</h6>
-                                <p className="font-light text-xs text-center text-blue-500 uppercase">{texts.projectsResponsive}</p>
-                            </div>
-                            <div className="flex flex-wrap place-content-center py-4 gap-2">
-                                <span className="px-2 py-1 font-light text-[10px] text-center bg-yellow-500/80 rounded-full">JavaScript</span>
-                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-cyan-500/80 rounded-full">React</span>
-                                <span className="px-2 py-1 font-light text-[10px] text-center bg-violet-400/80 rounded-full">Redux</span>
-                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
-                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-green-600/80 rounded-full">Node.js</span>
-                                <span className="px-2 py-1 font-light text-[10px] text-center bg-gray-200/80 rounded-full">Express</span>
-                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-blue-700/80 rounded-full">PostgreSQL</span>
-                                <span className="px-2 py-1 font-light text-[10px] text-center bg-blue-300/80 rounded-full">Sequelize</span>
-                            </div>
-                        </div>
-                        <div className="flex mt-4 justify-center items-center gap-4">
-                            <span className="p-2 rounded-full shadow-sm shadow-pink-200 cursor-pointer hover:shadow-md hover:shadow-pink-300 ease-in-out duration-200">
-                                <a href="https://github.com/franodangelo/Videogames-Proyect" target="_blank" rel="noreferrer"><FaGithub /></a>
-                            </span>
-                            <span className="p-2 rounded-full shadow-sm shadow-pink-200 cursor-pointer hover:shadow-md hover:shadow-pink-300 ease-in-out duration-200">
-                                <a href="https://videogamesspa.vercel.app/" target="_blank" rel="noreferrer"><IoMdOpen /></a>
-                            </span>
-                        </div>
-                    </div>
+                <section className="grid grid-cols-1 p-4 gap-8 md:grid-cols-3">
                     <div className="flex flex-col h-auto w-full p-8 items-center justify-center shadow-md shadow-blue-200 rounded-xl relative cursor-pointer hover:shadow-lg hover:shadow-blue-300 ease-in-out duration-200">
                         <span className="flex mb-4 py-1 px-2 self-end text-xs text-white bg-blue-600/50 rounded-md uppercase">Sep 22</span>
-                        <Image className="rounded" src={propertyImg5} alt="project thumbnail" />
+                        <Image className="rounded object-cover" height={200} width={400} src={propertyImg5} alt="project thumbnail" />
                         <div>
                             <div className="pb-4">
                                 <h6 className="mt-4 py-2 text-center border-t-2 border-gray-200">Tennis E-commerce</h6>
@@ -82,7 +48,7 @@ export default function Projects() {
                             <span className="flex mb-4 py-1 px-2 self-end text-xs text-blue-800 bg-blue-200/50 rounded-md uppercase">API REST</span>
                             <span className="flex mb-4 py-1 px-2 self-end text-xs text-white bg-blue-600/50 rounded-md uppercase">{texts.projectsAug} 22</span>
                         </div>
-                        <Image className="rounded" src={propertyImg4} alt="project thumbnail" />
+                        <Image className="rounded object-cover" height={200} width={400} src={propertyImg4} alt="project thumbnail" />
                         <div>
                             <div className="pb-4">
                                 <h6 className="mt-4 py-2 text-center border-t-2 border-gray-200">Small Weather App</h6>
@@ -105,7 +71,7 @@ export default function Projects() {
                     </div>
                     <div className="flex flex-col h-auto w-full p-8 items-center justify-center shadow-md shadow-blue-200 rounded-xl relative cursor-pointer hover:shadow-lg hover:shadow-blue-300 ease-in-out duration-200">
                         <span className="flex mb-4 py-1 px-2 self-end text-xs text-white bg-blue-600/50 rounded-md uppercase">{texts.projectsAug} 22</span>
-                        <Image className="rounded" src={propertyImg3} alt="project thumbnail" />
+                        <Image className="rounded object-cover" height={200} width={400} src={propertyImg3} alt="project thumbnail" />
                         <div>
                             <div className="pb-4">
                                 <h6 className="mt-4 py-2 text-center border-t-2 border-gray-200">Services SPA</h6>
@@ -131,7 +97,7 @@ export default function Projects() {
                             <span className="flex mb-4 py-1 px-2 self-end text-xs text-blue-800 bg-blue-200/50 rounded-md uppercase">API REST</span>
                             <span className="flex mb-4 py-1 px-2 self-end text-xs text-white bg-blue-600/50 rounded-md uppercase">Jun 22</span>
                         </div>
-                        <Image className="rounded" src={propertyImg2} alt="project thumbnail" />
+                        <Image className="rounded object-cover" height={200} width={400} src={propertyImg2} alt="project thumbnail" />
                         <div>
                             <div className="pb-4">
                                 <h6 className="mt-4 py-2 text-center border-t-2 border-gray-200">yumPaw: Pet Care & Shop</h6>
@@ -154,6 +120,40 @@ export default function Projects() {
                             </span>
                             <span className="p-2 rounded-full shadow-sm shadow-pink-200 cursor-pointer hover:shadow-md hover:shadow-pink-300 ease-in-out duration-200">
                                 <a href="https://client-pg.vercel.app/" target="_blank" rel="noreferrer"><IoMdOpen /></a>
+                            </span>
+                        </div>
+                    </div>
+                    <div className="flex flex-col h-full w-full p-8 items-center justify-center shadow-md shadow-blue-200 rounded-xl relative cursor-pointer hover:shadow-lg hover:shadow-blue-300 ease-in-out duration-200">
+                        <div className="w-full flex justify-between">
+                            {/* <span className="w-fit flex mb-4 py-1 px-2 self-end text-xs text-white bg-pink-500 rounded-md uppercase">Updated</span> */}
+                            <div className="w-full flex justify-end gap-2">
+                                <span className="flex mb-4 py-1 px-2 self-end text-xs text-blue-800 bg-blue-200/50 rounded-md uppercase">API REST</span>
+                                <span className="flex mb-4 py-1 px-2 self-end text-xs text-white bg-blue-600/50 rounded-md uppercase">May 22</span>
+                            </div>
+                        </div>
+                        <Image className="rounded object-cover" height={200} width={400} src={propertyImg1} alt="project thumbnail" />
+                        <div>
+                            <div className="pb-4">
+                                <h6 className="mt-4 py-2 text-center border-t-2 border-gray-200">Gameon: Video games Project</h6>
+                                <p className="font-light text-xs text-center text-blue-500 uppercase">{texts.projectsResponsive}</p>
+                            </div>
+                            <div className="flex flex-wrap place-content-center py-4 gap-2">
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-yellow-500/80 rounded-full">JavaScript</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-cyan-500/80 rounded-full">React</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-violet-400/80 rounded-full">Redux</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-emerald-500/80 rounded-full">Tailwind</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-green-600/80 rounded-full">Node.js</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-gray-200/80 rounded-full">Express</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center text-white bg-blue-700/80 rounded-full">PostgreSQL</span>
+                                <span className="px-2 py-1 font-light text-[10px] text-center bg-blue-300/80 rounded-full">Sequelize</span>
+                            </div>
+                        </div>
+                        <div className="flex mt-4 justify-center items-center gap-4">
+                            <span className="p-2 rounded-full shadow-sm shadow-pink-200 cursor-pointer hover:shadow-md hover:shadow-pink-300 ease-in-out duration-200">
+                                <a href="https://github.com/franodangelo/Videogames-Proyect" target="_blank" rel="noreferrer"><FaGithub /></a>
+                            </span>
+                            <span className="p-2 rounded-full shadow-sm shadow-pink-200 cursor-pointer hover:shadow-md hover:shadow-pink-300 ease-in-out duration-200">
+                                <a href="https://videogamesspa.vercel.app/" target="_blank" rel="noreferrer"><IoMdOpen /></a>
                             </span>
                         </div>
                     </div>

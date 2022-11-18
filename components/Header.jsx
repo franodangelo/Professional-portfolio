a4b7c5cd7e076badea331b087ab755e9656a63cc
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-11-18 19:19:42 -0300

    section cleaned up with new style classes

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,30 +4,35 @@ import { LanguageContext } from "../context/LanguageContext";
 import { FaLinkedinIn, FaGithub, FaTwitter } from "react-icons/fa";
 
 export default function Header() {
-
     const { texts } = useContext(LanguageContext);
 
     return (
         <header id="header" className="w-full h-screen text-center transition-all">
             <main className="flex w-full h-full mx-auto justify-center items-center">
-                <section className="flex flex-col w-[90%]">
+                <section className="flex flex-col">
                     <h6 className="sectionTypeHeading">🚀 {texts.headerSection}</h6>
-                    <h2 className="py-4 font-extrabold text-blue-600 dark:text-white">{texts.headerH1}</h2>
+                    <h2>{texts.headerH1}</h2>
                     <h4 className="py-2 font-normal text-slate-800 dark:text-slate-300">{texts.headerH2} 🇦🇷</h4>
                     <div className="flex m-auto my-8 items-end justify-between gap-8 md:gap-16">
-                        <div className="p-4 rounded-full shadow-md shadow-blue-200 dark:shadow-slate-800 cursor-pointer hover:scale-105 hover:shadow-pink-300 dark:hover:shadow-slate-700 ease-in-out duration-200">
+                        <div className="socialButton">
                             <Link href="/">
-                                <a href="https://www.linkedin.com/in/franco-dangelo/" target="_blank" rel="noreferrer"><FaLinkedinIn className="dark:text-slate-200" /></a>
+                                <a href="https://www.linkedin.com/in/franco-dangelo/" target="_blank" rel="noreferrer">
+                                    <FaLinkedinIn className="dark:text-slate-200" />
+                                </a>
                             </Link>
                         </div>
-                        <div className="p-4 rounded-full shadow-md shadow-blue-200 dark:shadow-slate-800 cursor-pointer hover:scale-105 hover:shadow-pink-300 dark:hover:shadow-slate-700 ease-in-out duration-200">
+                        <div className="socialButton">
                             <Link href="">
-                                <a href="https://github.com/franodangelo" target="_blank" rel="noreferrer"><FaGithub className="dark:text-slate-200" /></a>
+                                <a href="https://github.com/franodangelo" target="_blank" rel="noreferrer">
+                                    <FaGithub className="dark:text-slate-200" />
+                                </a>
                             </Link>
                         </div>
-                        <div className="p-4 rounded-full shadow-md shadow-blue-200 dark:shadow-slate-800 cursor-pointer hover:scale-105 hover:shadow-pink-300 dark:hover:shadow-slate-700 ease-in-out duration-200">
+                        <div className="socialButton">
                             <Link href="">
-                                <a href="https://twitter.com/techfrano" target="_blank" rel="noreferrer"><FaTwitter className="dark:text-slate-200" /></a>
+                                <a href="https://twitter.com/techfrano" target="_blank" rel="noreferrer">
+                                    <FaTwitter className="dark:text-slate-200" />
+                                </a>
                             </Link>
                         </div>
                     </div>

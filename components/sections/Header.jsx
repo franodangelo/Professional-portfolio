60ee206ec94fe2586a26777e7cdede56af9936ed
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-21 10:56:23 -0300

    cleaned up header

--- components/sections/Header.jsx
+++ components/sections/Header.jsx
@@ -8,24 +8,22 @@ export default function Header() {
 
     return (
         <header id="header" className="w-full h-screen text-center transition-all">
-            <main className="flex w-full h-full mx-auto p-6 md:p-10 justify-center items-center">
-                <section className="flex flex-col w-full items-center">
-                    <div className="flex flex-col">
-                        <motion.h1 animate={{scale: 1}} initial={{scale: 0}}>{texts.headerH1}</motion.h1>
-                        <motion.p animate={{x: 0}} transition={{type: "tween", duration: 3}} initial={{x: 4000}} className="flex self-end -mt-4 text-[10px] md:text-xs">{texts.headerNickname}</motion.p>
-                    </div>
-                    <h5 className="md:w-[80%] py-4 text-slate-800 dark:text-slate-300">{texts.headerH2}</h5>
-                    <div className="flex m-auto my-4 md:my-6 items-end justify-between gap-2 md:gap-6">
-                        <button className="secondaryButton gap-2">
-                            <FaLinkedinIn className="dark:text-slate-200" /> View Linkedin
-                        </button>
-                        <button className="secondaryButton gap-2">
-                            <FaGithub className="dark:text-slate-200" /> View Github
-                        </button>
-                    </div>
-                    <p>{texts.headerCTA}</p>
-                    <FaChevronDown className="animate-bounce my-4" />
-                </section>
+            <main className="flex flex-col w-full h-full p-6 md:p-10 justify-center items-center">
+                <div className="flex flex-col mt-20">
+                    <motion.h1 animate={{ scale: 1 }} initial={{ scale: 0 }}>{texts.headerH1}</motion.h1>
+                    <motion.p animate={{ x: 0 }} transition={{ type: "tween", duration: 3 }} initial={{ x: 4000 }} className="flex self-end -mt-4 text-[10px] md:text-xs">{texts.headerNickname}</motion.p>
+                </div>
+                <h5 className="md:w-[80%] py-4 text-slate-800 dark:text-slate-300">{texts.headerH2}</h5>
+                <div className="flex m-auto my-4 md:my-6 items-end justify-between gap-2 md:gap-6">
+                    <button className="secondaryButton gap-2">
+                        <FaLinkedinIn className="dark:text-slate-200" /> View Linkedin
+                    </button>
+                    <button className="secondaryButton gap-2">
+                        <FaGithub className="dark:text-slate-200" /> View Github
+                    </button>
+                </div>
+                <p>{texts.headerCTA}</p>
+                <FaChevronDown className="animate-bounce my-4" />
             </main>
         </header>
     )

6e645f9f6e702e290caf402109f460911ee16b01
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-03 21:39:11 -0300

    new chip for sass

--- components/cards/WorkingProjectCard.jsx
+++ components/cards/WorkingProjectCard.jsx
@@ -13,6 +13,7 @@ export default function WorkingProjectCard({ title, date, api, technologies }) {
                 {technologies?.includes("Redux") ? <span className="techChip dark:text-slate-900 bg-violet-400/80 dark:bg-violet-400">Redux</span> : null}
                 {technologies?.includes("Tailwind") ? <span className="techChip text-white bg-emerald-500/80 dark:bg-emerald-500">Tailwind</span> : null}
                 {technologies?.includes("CSS") ? <span className="techChip text-white bg-blue-500/80 dark:bg-blue-500">CSS</span> : null}
+                {technologies?.includes("Sass") ? <span className="techChip text-white bg-pink-500/80 dark:bg-pink-500">Sass</span> : null}
                 {technologies?.includes("Node.js") ? <span className="techChip text-white bg-green-600/80 dark:bg-green-600">Node.js</span> : null}
                 {technologies?.includes("Express") ? <span className="techChip dark:text-slate-900 bg-slate-200/80 dark:bg-slate-200">Express</span> : null}
                 {technologies?.includes("Firebase") ? <span className="techChip dark:text-slate-900 bg-yellow-500/80 dark:bg-yellow-400">Firebase</span> : null}

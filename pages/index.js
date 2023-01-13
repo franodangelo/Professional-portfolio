4a5572d924f523ac1a1d5799b1ebb9c44e3b1dc1
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-13 13:49:46 -0300

    removed experience section

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,6 @@ export default function Home() {
                 <Navbar />
                 <Header />
                 <Myself />
-                <Experience/>
                 <Projects />
                 <Skills />
                 <Contact />

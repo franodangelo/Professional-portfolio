1a5b1040313a845da704b731de2b99d680cbae0d
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-20 19:44:31 -0300

    better structure

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,38 +10,38 @@ export default function Footer() {
 
     return (
         <footer className="footer">
-            <div className="flex flex-wrap flex-col lg:flex-nowrap justify-center items-center gap-2">
-                <h4 className="footerHeading">{texts.footerCreated}</h4>
-                <ul className="flex justify-between lg:justify-evenly gap-2 lg:gap-4">
-                    <span className="footerSocial">
-                        <Image
-                            src={next}
-                            alt="Next.js logo"
-                            width="24px"
-                            height="24px"
-                        />
-                    </span>
-                    <span className="footerSocial">
-                        <Image
-                            src={react}
-                            alt="React logo"
-                            width="24px"
-                            height="24px"
-                        />
-                    </span>
-                    <span className="footerSocial">
-                        <Image
-                            src={tailwind}
-                            alt="Tailwind CSS logo"
-                            width="24px"
-                            height="24px"
-                        />
-                    </span>
-                </ul>
-            </div>
-            <div className="flex flex-col items-center gap-2">
+            <div className="flex flex-col items-center md:items-start gap-2">
                 <h4 className="footerHeading">{texts.footerThanks} 😀</h4>
                 <p className="footerSubheading">{texts.footerRights}</p>
+            </div>
+            <div className="flex flex-wrap flex-col lg:flex-nowrap justify-center items-center gap-2">
+                <h4 className="footerSubheading">{texts.footerCreated}</h4>
+                <ul className="flex justify-between lg:justify-evenly gap-2 lg:gap-4">
+                    <span className="footerSocial">
+                        <div className="flex w-6 h-6 md:w-4 md:h-4">
+                            <Image
+                                src={next}
+                                alt="Next.js logo"
+                            />
+                        </div>
+                    </span>
+                    <span className="footerSocial">
+                        <div className="flex w-6 h-6 md:w-4 md:h-4">
+                            <Image
+                                src={react}
+                                alt="React logo"
+                            />
+                        </div>
+                    </span>
+                    <span className="footerSocial">
+                        <div className="flex w-6 h-6 md:w-4 md:h-4">
+                            <Image
+                                src={tailwind}
+                                alt="Tailwind CSS logo"
+                            />
+                        </div>
+                    </span>
+                </ul>
             </div>
         </footer>
     );

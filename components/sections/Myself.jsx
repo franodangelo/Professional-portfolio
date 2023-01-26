70279577197623cc72329e74842c5c444aee2554
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-26 15:24:43 -0300

    changes in chips

--- components/sections/Myself.jsx
+++ components/sections/Myself.jsx
@@ -29,7 +29,7 @@ export default function Myself() {
                 </div>
                 <section className="col-span-2">
                     <div className="flex gap-2">
-                        <span className="roleChip bg-blue-300 dark:bg-blue-400 text-slate-900">Full-Stack</span>
+                        <span className="roleChip bg-blue-300 dark:bg-blue-400 text-slate-900">Full Stack</span>
                         <span className="roleChip bg-blue-200 dark:bg-blue-300 text-slate-900">Frontend</span>
                         <span className="roleChip bg-pink-200 text-slate-600">{texts.myselfProfile}</span>
                     </div>
